db9ac11b1f3c4bf2aba9bad551b28bb92f8ced30
JeanMarko173 <jeanmarko.13.7@gmail.com>
2021-12-09 15:23:54 -0500

    Agrega Test to counter

--- Semana 4/Redux/my-app/src/App.test.js
+++ Semana 4/Redux/my-app/src/App.test.js
@@ -1,8 +1,28 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './store/index'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Add +1', () => {
+  render(<Provider store={store}>
+    <App />
+  </Provider>);
+
+  const buttonElement = screen.getByText(/Increment/);
+  buttonElement.click(buttonElement);
+
+  const outputElement = screen.getByText(1);
+  expect(outputElement.textContent).toBe("1")
+});
+
+test('Decrease -1', () => {
+  render(<Provider store={store}>
+    <App />
+  </Provider>);
+
+  const buttonElement = screen.getByText(/Decrement/);
+  buttonElement.click(buttonElement);
+
+  const outputElement = screen.getByText(0);
+  expect(outputElement.textContent).toBe("0")
 });
